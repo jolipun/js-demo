1bca31e1af884afb2ffbe90fa110becac2b9347a
Jolita Pundzaite <jolita.pundzaite@gmail.com>
2020-04-04 16:43:44 +0300

    Styling DOM elements

--- home.js
+++ home.js
@@ -1,5 +1,13 @@
-let price = 49.99,
-    name = 'Hiking boots',
-    discounted = false;
+/* let person = {
+    name: 'John',
+    age: 23,
+    partTime: false,
+    showInfo: function(realAge) {
+        showMessage(this.name + ' is ' + realAge)
+    }
+};
 
-showMessage(name);
+console.log(person.showInfo(23)); */
+
+let header = document.getElementById('message');
+header.style.fontWeight = '100';
