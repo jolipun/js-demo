bdb3ce47c0a7dbafdc6066f8dc859c189c04b791
Jolita Pundzaite <jolita.pundzaite@gmail.com>
2020-04-04 16:54:39 +0300

    Detecting button clicks using eventlistener

--- home.js
+++ home.js
@@ -9,5 +9,11 @@
 
 console.log(person.showInfo(23)); */
 
-let header = document.getElementById('message');
-header.style.fontWeight = '100';
+/* let header = document.getElementById('message');
+header.style.fontWeight = '100'; */
+
+let button = document.getElementById('see-review');
+
+button.addEventListener('click', function() {
+    console.log('click');
+})
